d778b7176c690925d081f89a857f0ee7471d5f2a
Rex Osariemen <rexosariemen@gmail.com>
2021-01-23 16:51:22 -0500

    add bootstrap cdn to index.html page to apply styles

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React, { useState } from 'react';
 import useFetchJobs from './useFetchJobs';
 import { Container } from 'react-bootstrap';
+import Job from './Job';
 
 
 
@@ -14,10 +15,10 @@ function App() {
     <Container>
       {loading && <h1>Loading...</h1>}
       {error && <h1>Error...Please refresh your page!</h1>}
-      {jobs.length}
-      {/* {jobs.map(job => {
+      {/* {jobs.length} */}
+      {jobs.map(job => {
         return <Job key={job.id} job={job} />
-      })} */}
+      })}
     </Container>
   );
 }
