67f1391307f185a64eeda07fab48cbbaf0f45d8f
Rex Osariemen <rexosariemen@gmail.com>
2021-01-23 17:30:24 -0500

    implement collapse component to display job details

--- src/App.js
+++ src/App.js
@@ -1,6 +1,8 @@
 import React, { useState } from 'react';
 import useFetchJobs from './useFetchJobs';
 import { Container } from 'react-bootstrap';
+
+import JobsPagination from './JobsPagination';
 import Job from './Job';
 
 
@@ -12,7 +14,9 @@ function App() {
   const { jobs, loading, error } = useFetchJobs();
 
   return (
-    <Container>
+    <Container className='my-4'>
+      <h1 className='mb-4'>GitHub Jobs</h1>
+      <JobsPagination />
       {loading && <h1>Loading...</h1>}
       {error && <h1>Error...Please refresh your page!</h1>}
       {/* {jobs.length} */}
