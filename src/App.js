642898436f3fcdc8ca986802a82c7e8cbbded10e
Rex Osariemen <rexosariemen@gmail.com>
2021-01-23 16:28:55 -0500

    fetch data from api and handle CORS using heroku everywhere url

--- src/App.js
+++ src/App.js
@@ -1,10 +1,24 @@
-import './App.css';
+import React, { useState } from 'react';
+import useFetchJobs from './useFetchJobs';
+import { Container } from 'react-bootstrap';
+
+
 
 function App() {
+  const [params, setParams] = useState({});
+  const [page, setPage] = useState(1);
+
+  const { jobs, loading, error } = useFetchJobs();
+
   return (
-    <div className="App">
-      <h3>Hello World!</h3>
-    </div>
+    <Container>
+      {loading && <h1>Loading...</h1>}
+      {error && <h1>Error...Please refresh your page!</h1>}
+      {jobs.length}
+      {/* {jobs.map(job => {
+        return <Job key={job.id} job={job} />
+      })} */}
+    </Container>
   );
 }
 
